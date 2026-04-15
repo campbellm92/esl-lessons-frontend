937396f0650a02463e8f8fe6f70807078aacdb98
Matthew Campbell <m.campbell92@gmail.com>
2026-04-15 17:42:27 +0200

    feat: add additional elements

--- src/components/global/Card.tsx
+++ src/components/global/Card.tsx
@@ -1,24 +1,44 @@
-/*
-title: The title of the lesson
-image: An accompanying image for the lesson
-description: A description of the lesson
-*/
+import { Button } from "./Button";
+import { Badge } from "./Badge";
 
 interface CardProps {
     id: string;
+    img?: HTMLImageElement;
     title: string;
-    image?: HTMLImageElement;
-    description: string;
     level?: string[];
+    tags: string[];
+    description: string;
     onSelect: (id: string) => void
 }
 
-const cardBaseStyles = "flex gap-2 bg-amber-300 hover:bg-amber-200 text-zinc-800 text-2xl font-extrabold p-2 cursor-pointer border-solid border-2 border-zinc-100"
+const cardBaseStyles =
+    `flex flex-col gap-2 
+    max-w-md w-full
+    p-2
+    border-solid border-2 border-pumpkin-spice rounded-md`
 
-export const Card = ({ id, title, image, description, level, onSelect }: CardProps) => {
+const cardImgStyles =
+    `
+    
+    `
+const cardLevelStyles =
+    `
+    font-bold
+    text-pumpkin-spice
+
+    `
+
+export const Card = ({ id, img, title, level, description, tags, onSelect }: CardProps) => {
     return <div className={`${cardBaseStyles}`} onClick={() => onSelect(id)}>
-        <img src="" alt="" />
+        <img src={`${img}`} alt={`${title}`} className={cardImgStyles} />
         <h3>{title}</h3>
+        <p className={cardLevelStyles}>{level}</p>
+        <div className="flex flex-wrap gap-2">
+            {tags.map((tag) => (
+                <Badge key={tag} text={tag} />
+            ))}
+        </div>
         <p>{description}</p>
+        <div className="flex justify-center"><Button className="w-1/2">Learn</Button></div>
     </div>
 }
