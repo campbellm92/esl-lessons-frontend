d265dda6ece39c9685fd993f1e8b9b840d4fa424
Matthew Campbell <m.campbell92@gmail.com>
2026-04-16 22:15:24 +0200

    feat: add routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home"
+// import { Button } from "./components/global/Button"
+// import { Card } from "./components/global/Card"
+
+
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+
+
+
 
     </>
   )
